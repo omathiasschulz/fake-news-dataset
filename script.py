68f91c85ca50842ed629e743d0d4990b7d3a9a6b
Mathias Artur Schulz <mathiasschulz34@gmail.com>
2020-10-12 19:42:11 -0300

    [UPD] Adicionado a criação do CSV com o id, fake_news e text

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
-import wget
+import requests
+import time
+import pandas as pd
 
 # Repositório Fake.br-Corpus com os textos em TXT: https://github.com/roneysco/Fake.br-Corpus
 URL = 'https://raw.githubusercontent.com/roneysco/Fake.br-Corpus/master/'
@@ -8,25 +10,75 @@ TEXT_TRUE_META_INFORMATION = 'full_texts/true-meta-information/'
 TEXT_FALSE = 'full_texts/false/'
 TEXT_FAKE_META_INFORMATION = 'full_texts/fake-meta-information/'
 
+# Número de notícias falsas e verdadeiras
 TEXT_NUMBER = 3602
+TEXT_NUMBER = 3
 
-# # pip install wget
-# a = wget.download(URL + TEXT_TRUE + '1.txt')
+# Criação da DataFrame
+columns = ['ID', 'fake_news', 'text']
+# fake_news => 1 = True, 0 = False
+df = pd.DataFrame(columns = columns)
 
-# print(a)
+print('Iniciano a criação do CSV')
+
+inicio = time.time()
+falhas = 0
+falhasText = []
+
+# Gera as notícias verdadeiras
+for i in range(1, TEXT_NUMBER + 1):
+    print('Texto: %i' %(i))
+    req = requests.get(URL + TEXT_TRUE + str(i) + '.txt')
+    if req.status_code == requests.codes.ok:
+        df = df.append({'ID': i, 'fake_news': 0, 'text': req.text}, ignore_index=True)
+    else:
+        falhas += 1
+        falhasText.append(i)
+
+# Gera as notícias falsas
+for i in range(1, TEXT_NUMBER + 1):
+    print('Texto: %i' %(i))
+    req = requests.get(URL + TEXT_FALSE + str(i) + '.txt')
+    if req.status_code == requests.codes.ok:
+        df = df.append({'ID': i, 'fake_news': 1, 'text': req.text}, ignore_index=True)
+    else:
+        falhas += 1
+        falhasText.append(i)
+fim = time.time()
+
+# Realiza a criação do CSV
+df.to_csv('dataset.csv')
+
+print('CSV criado com sucesso! ')
+print('Número de falhas: %i' %(falhas))
+print('Falhas nos textos: ', falhasText)
+print('Tempo de execução: %f minutos' %((fim - inicio) / 60))
 
 
-import base64
-import requests
 
-ABC = URL + TEXT_TRUE + '1.txt'
-req = requests.get(ABC)
-print(req.text)
-# if req.status_code == requests.codes.ok:
-#     req = req.json()  # the response is a JSON
-#     # req is now a dict with keys: name, encoding, url, size ...
-#     # and content. But it is encoded with base64.
-#     content = base64.decodestring(req['content'])
-#     print(content)
-# else:
-#     print('Content was not found.')
+##### COLUNAS #####
+# author
+# link
+# category
+# date of publication
+# number of tokens
+# number of words without punctuation
+# number of types
+# number of links inside the news
+# number of words in upper case
+# number of verbs
+# number of subjuntive and imperative verbs
+# number of nouns
+# number of adjectives
+# number of adverbs
+# number of modal verbs (mainly auxiliary verbs)
+# number of singular first and second personal pronouns
+# number of plural first personal pronouns
+# number of pronouns
+# pausality
+# number of characters
+# average sentence length
+# average word length
+# percentage of news with speeling errors
+# emotiveness
+# diversity
