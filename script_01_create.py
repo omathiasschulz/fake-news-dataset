749f1cb40ffee153e3c2f9efa855f14ad7a094c7
Mathias Schulz <MathiasSchulz34@gmail.com>
2020-12-22 10:38:44 -0300

    [UPD] Adicionado alguns ajustes no script de criação

--- script_01_create.py
+++ script_01_create.py
@@ -1,32 +1,40 @@
-import requests, time
+import requests
+import time
 import pandas as pd
 
-# Repositório Fake.br-Corpus com os textos em TXT: https://github.com/roneysco/Fake.br-Corpus
+# repositório Fake.br-Corpus com os textos em TXT: https://github.com/roneysco/Fake.br-Corpus
 URL = 'https://raw.githubusercontent.com/roneysco/Fake.br-Corpus/master/'
-
+# rotas para as notícias e os metadatas
 TEXT_TRUE = 'full_texts/true/'
 TEXT_TRUE_META_INFORMATION = 'full_texts/true-meta-information/'
 TEXT_FAKE = 'full_texts/fake/'
 TEXT_FAKE_META_INFORMATION = 'full_texts/fake-meta-information/'
-
-# Número de notícias falsas e verdadeiras
+# número de notícias falsas e verdadeiras
 TEXT_NUMBER = 3602
 
+
 def requestMetaInformation(rota, index):
-    '''
+    """
     Método responsável por buscar o metadata da notícia determinada pelo index
-    '''
+
+    :param rota: Rota para buscar o metadata da notícia
+    :type rota: str
+    :param index: ID da notícia
+    :type index: int
+    :return: Retorna o metadata da notícia
+    :rtype: dict
+    """
     req = requests.get(URL + rota + str(index) + '-meta.txt')
 
-    # Valida se o request ocorreu com sucesso
+    # valida se o request ocorreu com sucesso
     if req.status_code != requests.codes.ok:
         return None
 
-    # Converte o metadata para array
+    # converte o metadata para array
     metadata = req.text.split('\r\n')
-    # Se a quantidade de informações é diferente de 25 ocorreu algum erro
+    # se a quantidade de informações é diferente de 25 ocorreu algum erro
     if len(metadata) != 25:
-        raise Exception('Falha ao buscar o metadata da notícia %i' %index)
+        raise Exception('Falha ao buscar o metadata da notícia %i' % index)
 
     return {
         'author': metadata[0],
@@ -37,82 +45,97 @@ def requestMetaInformation(rota, index):
         'percent_speeling_errors': metadata[22],
     }
 
+
 def generateNews(df, fake_news):
-    '''
+    """
     Método responsável por gerar as notícias de acordo com os parâmetros fornecidos
-    '''
+
+    :param df: Dataframe no qual será armazenado as notícias
+    :type df: Dataframe
+    :param fake_news: Determina se as notícias para buscar serão fake ou não
+    :type fake_news: int
+    :return: Retorna o Dataframe e os casos que resultaram falhas para buscar
+    :rtype: list
+    """
     falhas = 0
-    falhasText = []
-    # Busca as rotas de acordo com o tipo de notícia
+    falhas_text = []
+    # busca as rotas de acordo com o tipo de notícia
     if fake_news == 0:
         rota = TEXT_TRUE
-        rotaMeta = TEXT_TRUE_META_INFORMATION
+        rota_meta = TEXT_TRUE_META_INFORMATION
     else:
         rota = TEXT_FAKE
-        rotaMeta = TEXT_FAKE_META_INFORMATION
+        rota_meta = TEXT_FAKE_META_INFORMATION
 
     for i in range(1, TEXT_NUMBER + 1):
-        # Realiza a busca da noticia i, caso resulte algum erro registra uma falha
+        # realiza a busca da noticia i, caso resulte algum erro registra uma falha
         req = requests.get(URL + rota + str(i) + '.txt')
         if req.status_code != requests.codes.ok:
             falhas += 1
-            falhasText.append(i)
+            falhas_text.append(i)
             continue
 
-        # Busca o metadata, caso resulte algum erro registra uma falha
-        metadata = requestMetaInformation(rotaMeta, i)
-        if metadata == None:
+        # busca o metadata, caso resulte algum erro registra uma falha
+        metadata = requestMetaInformation(rota_meta, i)
+        if metadata is None:
             falhas += 1
-            falhasText.append(i)
+            falhas_text.append(i)
             continue
 
-        print('Tipo %s - Notícia: %i' %(fake_news, i))
-        # Remove múltiplos espaços
+        print('Tipo %s - Notícia: %i' % (fake_news, i))
+        # remove múltiplos espaços
         text = ' '.join(req.text.split())
-        # Insere a notícia no dataframe
+        # insere a notícia no dataframe
         news = {**{'ID': i, 'fake_news': fake_news, 'text': text}, **metadata}
         df = df.append(news, ignore_index=True)
 
     return [
         df,
         falhas,
-        falhasText,
+        falhas_text,
     ]
 
-try:
+
+def main():
+    """
+    Método main do script
+    """
     print('Iniciando a criação do CSV')
     inicio = time.time()
     falhas = 0
-    falhasTextFake = []
-    falhasTextReal = []
+    falhas_text_fake = []
+    falhas_text_real = []
 
-    # Criação do DataFrame com as colunas iniciais
+    # criação do DataFrame com as colunas iniciais
     columns = ['ID', 'fake_news', 'text']
-    df = pd.DataFrame(columns = columns)
+    df = pd.DataFrame(columns=columns)
 
-    # Monta o dataframe com as falsas notícias (fake_news = 1)
+    # monta o dataframe com as falsas notícias (fake_news = 1)
     result = generateNews(df, 1)
     df = result[0]
     falhas += result[1]
-    falhasTextFake += result[2]
+    falhas_text_fake += result[2]
 
-    # Monta o dataframe com as verdadeiras notícias (fake_news = 0)
+    # monta o dataframe com as verdadeiras notícias (fake_news = 0)
     result = generateNews(df, 0)
     df = result[0]
     falhas += result[1]
-    falhasTextReal +=result[2]
+    falhas_text_real += result[2]
 
-    # Realiza a conversão de colunas com números para o tipo numérico
-    df[['ID', 'fake_news', 'average_word_length', 'percent_speeling_errors']] = df[['ID', 'fake_news', 'average_word_length', 'percent_speeling_errors']].apply(pd.to_numeric)
+    # realiza a conversão de colunas com números para o tipo numérico
+    columns_to_convert = ['ID', 'fake_news', 'average_word_length', 'percent_speeling_errors']
+    df[columns_to_convert] = df[columns_to_convert].apply(pd.to_numeric)
 
-    # Realiza a criação do CSV
+    # realiza a criação do CSV
     df.to_csv('dataset_unformatted.csv')
 
     fim = time.time()
     print('CSV criado com sucesso! ')
-    print('Número de falhas: %i' %(falhas))
-    print('Falhas nas verdadeiras notícias: %a' %falhasTextReal)
-    print('Falhas nas falsas notícias: %a' %falhasTextFake)
-    print('Tempo de execução: %f minutos' %((fim - inicio) / 60))
-except Exception as e:
-    print('Falha ao gerar CSV: %s' %str(e))
+    print('Número de falhas: %i' % falhas)
+    print('Falhas nas verdadeiras notícias: %a' % falhas_text_real)
+    print('Falhas nas falsas notícias: %a' % falhas_text_fake)
+    print('Tempo de execução: %.2f minutos' % ((fim - inicio) / 60))
+
+
+if __name__ == '__main__':
+    main()
