06f54571e2be0f0a0d93c745ec926e4d4c09a952
Mathias Schulz <MathiasSchulz34@gmail.com>
2020-12-22 13:58:19 -0300

    [UPD] Adicionado alguns ajustes no script de formatação e adicionado a criação de novos datasets

--- script_02_format.py
+++ script_02_format.py
@@ -1,82 +1,123 @@
-import re, time, nltk
+import re
+import time
+import nltk
 import pandas as pd
+
 nltk.download('stopwords')
 
-# Tamanho mínimo que os textos podem possuir no montagem do CSV de textos formatados
-TEXT_LENGTH = 300
+# tamanho mínimo que os textos podem possuir no montagem do CSV de textos formatados
+TEXT_LENGTH_DEFAULT = 300
+TEXT_LENGTH_FOR_TESTS = [100, 200]
+
 
 def textClean(text):
-    '''
+    """
     Método responsável por realizar a limpeza do texto passado como parâmetro
-    '''    
-    # Coloca o texto em lowercase
+
+    :param text: Texto que será formatado
+    :type text: str
+    :return: Retorna o texto formatado
+    :rtype: str
+    """
+    # coloca o texto em lowercase
     text = text.lower()
 
-    # Regex que altera os caracteres para um espaço em branco, exceto os caracteres: 
-    # Letras, algumas letras com acentos, números, e o espaço (é tratado mais à frente)
+    # regex que altera os caracteres para um espaço em branco, exceto os caracteres:
+    # letras, algumas letras com acentos, números, e o espaço (é tratado mais à frente)
     text = re.sub(r'[^a-z0-9áàâãéèêíïóôõöúçñ ]', r' ', text)
 
-    # Realiza um slip no espaço
+    # realiza um slip no espaço
     text = text.split()
 
-    # Remoção de palavras com apenas uma letra
+    # remoção de palavras com apenas uma letra
     text = [w for w in text if len(w) > 1]
 
-    # Remove stopwords
+    # remove stopwords
     stopwords = set(nltk.corpus.stopwords.words('portuguese'))
-    text = [w for w in text if not w in stopwords]
+    text = [w for w in text if w not in stopwords]
     text = ' '.join(text)
 
     return text
 
-def fixLenTexts(df):
-    '''
-    Método responsável por setar um tamanho fixo de palavras para os textos
-    '''
-    # Cria uma nova coluna com o número de palavras de cada texto
+
+def infoDFAntigo(df):
+    """
+    Método responsável por apresentar o df sem filtrar pelo tamanho dos textos
+    :param df: Dataframe para apresentar
+    :type df: Dataframe
+    """
+    # cria uma nova coluna com o número de palavras de cada texto
     df['number_words'] = df.apply(lambda row: len(row.text.split()), axis=1)
 
-    # Apresenta um esboço do CSV
+    # apresenta um esboço do CSV
     print(df.head())
-    print('Menor número de palavras de um texto: %i ' %df['number_words'].min())
-    print('Maior número de palavras de um texto: %i ' %df['number_words'].max())
+    print('Menor número de palavras de um texto: %i ' % df['number_words'].min())
+    print('Maior número de palavras de um texto: %i ' % df['number_words'].max())
 
     print('\n=> DataFrame sem tamanho mínino de palavras nos textos:')
-    print('Quantidade de textos: %i' %len(df))
-    print('Média de palavras dos textos: %i ' %df['number_words'].mean())
+    print('Quantidade de textos: %i' % len(df))
+    print('Média de palavras dos textos: %i ' % df['number_words'].mean())
 
-    # Removido textos com menos de TEXT_LENGTH palavras
-    df = df[df['number_words'] >= TEXT_LENGTH]
-    # Reajusta os indexs
+
+def fixLenTexts(df, text_length):
+    """
+    Método responsável por setar um tamanho fixo de palavras para os textos
+
+    :param df: Dataframe para ajustar o tamanho
+    :type df: Dataframe
+    :param text_length: Tamanho mínimo que os textos podem ter
+    :type text_length: int
+    :return: Retorna o Dataframe
+    :rtype: Dataframe
+    """
+    # removido textos com menos de TEXT_LENGTH palavras
+    df = df[df['number_words'] >= text_length]
+    # reajusta os indexs
     df = df.reset_index(drop=True)
 
     print('\nNOVO DATAFRAME')
-    print('=> DataFrame sem textos com menos de %i palavras' %TEXT_LENGTH)
-    print('Quantidade de textos: %i' %len(df))
-    print('Média de palavras dos textos: %i \n' %df['number_words'].mean())
+    print('=> DataFrame sem textos com menos de %i palavras' % text_length)
+    print('Quantidade de textos: %i' % len(df))
+    print('Média de palavras dos textos: %i \n' % df['number_words'].mean())
 
     return df
 
-try:
+
+def main():
+    """
+    Método main do script
+    """
     print('Iniciando a formatação do texto do CSV')
     inicio = time.time()
 
-    # Realiza a leitura do CSV com o texto não formatado
+    # realiza a leitura do CSV com o texto não formatado
     df = pd.read_csv('dataset_unformatted.csv', index_col=0)
 
-    # Realiza a pré-processamento do texto
+    # realiza a pré-processamento do texto
     print('Realizando a limpeza dos textos... ')
     df.text = df.apply(lambda x: textClean(x.text), axis=1)
 
-    # Seta um tamanho fixo de palavras para os textos
-    print('Ajustando o tamanho dos textos... ')
-    df = fixLenTexts(df)
+    # apresenta o df
+    infoDFAntigo(df)
 
-    # Realiza a criação do novo CSV
-    df.to_csv('dataset.csv')
+    # seta um tamanho fixo de palavras para os textos
+    print('Ajustando o tamanho dos textos... ')
+    # cria o dataset.csv com o tamanho default para os textos
+    df_length_default = fixLenTexts(df, TEXT_LENGTH_DEFAULT)
+    # realiza a criação do novo CSV
+    df_length_default.to_csv('dataset.csv')
+
+    # cria outros CSV's com os tamanhos para teste
+    for length in TEXT_LENGTH_FOR_TESTS:
+        df_test = fixLenTexts(df, length)
+
+        # realiza a criação do novo CSV para teste
+        df_test.to_csv('dataset_' + str(length) + '_palavras.csv')
 
     fim = time.time()
-    print('CSV com o texto formatado criado com sucesso! ')
-    print('Tempo de execução: %f minutos' %((fim - inicio) / 60))
-except Exception as e:
-    print('Falha ao gerar CSV: %s' %str(e))
+    print('CSVs com o texto formatado criados com sucesso! ')
+    print('Tempo de execução: %.2f minutos' % ((fim - inicio) / 60))
+
+
+if __name__ == '__main__':
+    main()
